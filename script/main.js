f6684695b476ad04d9996a977174e64762643a95
Iulia9777 <102027523+Iulia9777@users.noreply.github.com>
2022-07-04 13:39:52 +0300

    change 030722

--- script/main.js
+++ script/main.js
@@ -4,9 +4,9 @@ let toleft = 0;
 document.addEventListener('click', function(ev) {
     console.log(ev.target);
     if ( ev.target.className === 'down key' && totop < 250 ) {
-        if (toleft > 0 && toleft < 100 && totop >= 250 && totop <= 250) {
+        if (toleft > 0 && toleft < 100 && totop >= 150 && totop <= 150) {
         } else {
-            if (toleft > 100 && toleft < 200 && totop >= 100 && totop <= 200) {
+            if (toleft > 100 && toleft < 200 && totop >= 50 && totop <= 50) {
             } else {            
                 totop = totop + 10;
                 document.getElementsByClassName('square')[0].style.top = totop + 'px';
@@ -16,27 +16,27 @@ document.addEventListener('click', function(ev) {
     if (ev.target.className === 'up key' && totop > 0) {
         if (toleft > 0 && toleft < 100 && totop >= 200 && totop <= 250) {
         } else {
-            if (toleft > 100 && toleft < 200 && totop >= 100 && totop <= 100) {
+            if (toleft > 100 && toleft < 200 && totop >= 150 && totop <= 150) {
             } else {
                 totop = totop - 10;
                 document.getElementsByClassName('square')[0].style.top = totop + 'px';
             }
         }
     }
-    if (ev.target.className=='right key' && toleft<250) {
-        if (toleft >= 0 && toleft <= 0 && totop > 200 && totop < 250) {
+    if (ev.target.className=='right key' && toleft < 250) {
+        if (toleft >= 0 && toleft <= 0 && totop > 150 && totop < 250) {
         } else {
-            if (toleft >= 100 && toleft < 200 && totop > 100 && totop < 200) {
+            if (toleft >= 100 && toleft < 200 && totop > 50 && totop < 150) {
             } else {
                 toleft = toleft+10;
                 document.getElementsByClassName('square')[0].style.left = toleft+'px';
             }
         }
     }
-    if (ev.target.className=='left key' && toleft>0) {
-        if (toleft > 50 && toleft <= 100 && totop > 200 && totop < 250) {
+    if (ev.target.className=='left key' && toleft > 0) {
+        if (toleft > 50 && toleft <= 100 && totop > 150 && totop < 250) {
         } else {
-            if (toleft > 100 && toleft <= 200 && totop > 100 && totop < 200) {
+            if (toleft > 100 && toleft <= 200 && totop > 50 && totop < 150) {
             } else {
                 toleft = toleft-10;
                 document.getElementsByClassName('square')[0].style.left = toleft+'px';
